3e876afb2b880010aadd5b70429abb68b5cadaac
ksaint30 <san.kimtx@gmail.com>
2024-11-03 11:51:57 -0700

    try 6

--- code_12.py
+++ code_12.py
@@ -13,11 +13,9 @@ def get_fibonacci_number_sequence(number):
     if number == 1:
         return [1,1]
     list = get_fibonacci_number_sequence(number-1)
-    if len(list) < number:
-        return list
     list.append(list[-1] + list[-2])
-    return list
+    return list 
 
 if __name__ == "__main__":
-    print(get_fibonacci_number(4))
-    print(get_fibonacci_number_sequence(4))
+    print(get_fibonacci_number(7))
+    print(get_fibonacci_number_sequence(7))
