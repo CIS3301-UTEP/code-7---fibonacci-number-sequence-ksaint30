91cd469a7305533b586f754d0d48ef1eb21c304a
ksaint30 <san.kimtx@gmail.com>
2024-11-03 11:16:35 -0700

    try 4th

--- code_12.py
+++ code_12.py
@@ -9,9 +9,9 @@ def get_fibonacci_number(position):
 
 def get_fibonacci_number_sequence(number):
     if number == 0:
-        return [0]
+        return [1]
     if number == 1:
-        return [0,1]
+        return [1,1]
     list = get_fibonacci_number_sequence(number-1)
     list.append(list[-1] + list[-2])
     return list
